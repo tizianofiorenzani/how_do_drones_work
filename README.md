f2ba5c067097c5b070ab533182e55a94ccfa446b
Tiziano Fiorenzani <tiziano.f@xworks.com>
2017-09-27 08:40:46 -0500

    First

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# how_do_drones_work
+# How Do Drones Work
 A collection of scripts from my totorials
+
+* Follow my [youtube channel](https://www.youtube.com/watch?v=TFDWs_DG2QY&index=2&list=PLuteWQUGtU9BcXXr3jCG00uVXFwQJkLRa)
+* Find  me on [Linkedin](www.linkedin.com/in/tiziano-fiorenzani-0a856415)
+* Read my complete series on [How Do Drones Work](https://www.linkedin.com/pulse/how-do-drones-work-part-1-introduction-tiziano-fiorenzani/)
---
+++ scripts/dronekit_test_02.py
@@ -0,0 +1,151 @@
+"""
+Simple script for take off and control with arrow keys
+"""
+
+
+import time
+from dronekit import connect, VehicleMode, LocationGlobalRelative, Command, LocationGlobal
+from pymavlink import mavutil
+
+#- Importing Tkinter: sudo apt-get install python-tk
+import Tkinter as tk
+
+
+#-- Connect to the vehicle
+print('Connecting...')
+vehicle = connect('udp:127.0.0.1:14551')
+
+#-- Setup the commanded flying speed
+gnd_speed = 5 # [m/s]
+
+#-- Define arm and takeoff
+def arm_and_takeoff(altitude):
+
+   while not vehicle.is_armable:
+      print("waiting to be armable")
+      time.sleep(1)
+
+   print("Arming motors")
+   vehicle.mode = VehicleMode("GUIDED")
+   vehicle.armed = True
+
+   while not vehicle.armed: time.sleep(1)
+
+   print("Taking Off")
+   vehicle.simple_takeoff(altitude)
+
+   while True:
+      v_alt = vehicle.location.global_relative_frame.alt
+      print(">> Altitude = %.1f m"%v_alt)
+      if v_alt >= altitude - 1.0:
+          print("Target altitude reached")
+          break
+      time.sleep(1)
+      
+ #-- Define the function for sending mavlink velocity command in body frame
+def set_velocity_body(vehicle, vx, vy, vz):
+    """ Remember: vz is positive downward!!!
+    http://ardupilot.org/dev/docs/copter-commands-in-guided-mode.html
+    
+    Bitmask to indicate which dimensions should be ignored by the vehicle 
+    (a value of 0b0000000000000000 or 0b0000001000000000 indicates that 
+    none of the setpoint dimensions should be ignored). Mapping: 
+    bit 1: x,  bit 2: y,  bit 3: z, 
+    bit 4: vx, bit 5: vy, bit 6: vz, 
+    bit 7: ax, bit 8: ay, bit 9:
+    
+    
+    """
+    msg = vehicle.message_factory.set_position_target_local_ned_encode(
+            0,
+            0, 0,
+            mavutil.mavlink.MAV_FRAME_BODY_NED,
+            0b0000111111000111, #-- BITMASK -> Consider only the velocities
+            0, 0, 0,        #-- POSITION
+            vx, vy, vz,     #-- VELOCITY
+            0, 0, 0,        #-- ACCELERATIONS
+            0, 0)
+    vehicle.send_mavlink(msg)
+    vehicle.flush()
+    
+    
+#-- Key event function
+def key(event):
+    if event.char == event.keysym: #-- standard keys
+        if event.keysym == 'r':
+            print("r pressed >> Set the vehicle to RTL")
+            vehicle.mode = VehicleMode("RTL")
+            
+    else: #-- non standard keys
+        if event.keysym == 'Up':
+            set_velocity_body(vehicle, gnd_speed, 0, 0)
+        elif event.keysym == 'Down':
+            set_velocity_body(vehicle,-gnd_speed, 0, 0)
+        elif event.keysym == 'Left':
+            set_velocity_body(vehicle, 0, -gnd_speed, 0)
+        elif event.keysym == 'Right':
+            set_velocity_body(vehicle, 0, gnd_speed, 0)
+    
+    
+#---- MAIN FUNCTION
+#- Takeoff
+arm_and_takeoff(10)
+ 
+#- Read the keyboard with tkinter
+root = tk.Tk()
+print(">> Control the drone with the arrow keys. Press r for RTL mode")
+root.bind_all('<Key>', key)
+root.mainloop()
+ 
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+      
